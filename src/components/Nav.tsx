a9273796279cd69ba4d1ad4f56952667f7d46f13
ASSANI Younes <younesassani@gmail.com>
2024-07-31 15:38:31 +0300

    update navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ export default function Nav() {
   return (
     <nav className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-center py-6">
-        <h1>QLT</h1>
+        <h1 className="text-3xl font-bold">QLT</h1>
       </div>
     </nav>
   );
