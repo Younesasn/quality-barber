4b3bafbd16ad8fdb78e67610e904c44c1590c729
ASSANI Younes <younesassani@gmail.com>
2024-07-31 18:26:32 +0300

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,16 @@
-import {
-    InstagramLogoIcon,
-  } from "@radix-ui/react-icons";
 import { Avatars } from "./Avatars";
-
-interface Icon {
-  icon: JSX.Element;
-  url: string;
-}
-
-const icons: Icon[] = [
-  { icon: <InstagramLogoIcon />, url: "https://www.instagram.com/qlt.barber/" },
-];
 
 export function Footer() {
   return (
     <footer className="px-5 lg:px-10 p-5 max-w-7xl mx-auto">
       <div className="flex flex-col gap-y-5 md:flex-row items-start md:items-center justify-between w-full gap-x-5">
-        <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-1">
           <img
             className="w-8 h-8 rounded-full"
-            src="https://magicui.design/icon.png"
+            src="/images/qlt.jpg"
             alt="Quality Barber Logo"
           />
-           <h2 className="font-bold text-white">
-              Quality Barber
-            </h2>
+          <h2 className="font-bold text-white">Quality Barber</h2>
         </div>
         <div className="flex items-center gap-x-4">
           <Avatars />
