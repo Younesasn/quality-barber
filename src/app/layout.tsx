fbbcbd07ecd47b7e8c09b873dd45535e369a1570
ASSANI Younes <younesassani@gmail.com>
2024-08-01 19:50:46 +0300

    remove indicators breackpoints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,26 +27,6 @@ export default function RootLayout({
         )}
       >
         {children}
-        <div className="flex items-center m-2 fixed bottom-0 right-0 border border-black rounded p-2 space-x-2 bg-black text-white text-sm">
-          <img
-            className="mx-auto h-5 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=white"
-            alt="Your Company"
-          />
-          <span className="ml-1 sm:hidden md:hidden lg:hidden xl:hidden">
-            default (&lt; 640px)
-          </span>
-          <span className="ml-1 hidden sm:inline md:hidden font-extrabold">
-            SM
-          </span>
-          <span className="ml-1 hidden md:inline lg:hidden font-extrabold">
-            MD
-          </span>
-          <span className="ml-1 hidden lg:inline xl:hidden font-extrabold">
-            LG
-          </span>
-          <span className="ml-1 hidden xl:inline font-extrabold">XL</span>
-        </div>
       </body>
     </html>
   );
