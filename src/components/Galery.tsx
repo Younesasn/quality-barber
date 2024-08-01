cffd0997e79aa8143ca1e53ce78b613cd14b19c0
ASSANI Younes <younesassani@gmail.com>
2024-08-01 13:40:06 +0300

    add motion in gallery

--- src/components/Galery.tsx
+++ src/components/Galery.tsx
@@ -1,6 +1,25 @@
+"use client";
+import { motion, useInView } from "framer-motion";
 import { ParallaxScroll } from "./ui/parallax-scroll";
+import { useRef } from "react";
 
 export function Galery() {
+  const fadeInRef = useRef(null);
+  const fadeInInView = useInView(fadeInRef, {
+    once: true,
+  });
+
+  const fadeUpVariants = {
+    initial: {
+      opacity: 0,
+      y: 24,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+    },
+  };
+
   const images = [
     "/images/galery/galery-1.jpg",
     "/images/galery/galery-2.jpg",
@@ -12,5 +31,20 @@ export function Galery() {
     "/images/galery/galery-8.jpg",
     "/images/galery/galery-9.jpg",
   ];
-  return <ParallaxScroll images={images} />;
+  return (
+    <motion.div
+      ref={fadeInRef}
+      animate={fadeInInView ? "animate" : "initial"}
+      variants={fadeUpVariants}
+      initial={false}
+      transition={{
+        duration: 0.6,
+        delay: 0.1,
+        ease: [0.21, 0.47, 0.32, 0.98],
+        type: "spring",
+      }}
+    >
+      <ParallaxScroll images={images} />
+    </motion.div>
+  );
 }
