d96b6fa705e7e4ea2d2e474f7b6754006a1740d8
ASSANI Younes <younesassani@gmail.com>
2024-09-20 01:02:24 +0200

    add pricing

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -172,6 +172,188 @@ const faqs = [
         ),
       },
       {
+        question: "Tresses Femme by Mouns",
+        answer: (
+          <ul className="space-y-4 text-sm px-1">
+            <li className="flex justify-between">
+              <div className="">2 nattes</div>
+              <div className="">
+                <span className="text-neutral-400">30min • </span> 20€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                2 nattes <span className="text-neutral-400">avec rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">1h • </span> 35€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">Tresses collées entières</div>
+              <div className="">
+                <span className="text-neutral-400">1h 30min • </span> 30€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Tresses collées entières{" "}
+                <span className="text-neutral-400">avec rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 30€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Bohemian braid{" "}
+                <span className="text-neutral-400">(braid + ondulation)</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 55€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Bohemian braid avec rajouts{" "}
+                <span className="text-neutral-400">(braid + ondulation)</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 75€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Knotless braid sans rajouts{" "}
+                <span className="text-neutral-400">(braid sans nœuds)</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 55€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Knotless braid avec rajouts{" "}
+                <span className="text-neutral-400">(braid sans nœuds)</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 75€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Tresses libres{" "}
+                <span className="text-neutral-400">sans rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">2h • </span> 35€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">Braid box </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 55€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Braid box <span className="text-neutral-400">avec rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 75€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">Vanilles </div>
+              <div className="">
+                <span className="text-neutral-400">2h • </span> 55€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Vanilles <span className="text-neutral-400">avec rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">3h • </span> 75€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Fulani braid{" "}
+                <span className="text-neutral-400">
+                  (braid box + tresses collées demi-tête)
+                </span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 55€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Fulani braid{" "}
+                <span className="text-neutral-400">
+                  (braid box + tresses collées demi-tête avec rajouts)
+                </span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">4h • </span> 75€
+              </div>
+            </li>
+          </ul>
+        ),
+      },
+      {
+        question: "Tresses Homme by Mouns",
+        answer: (
+          <ul className="space-y-4 text-sm px-1">
+            <li className="flex justify-between">
+              <div className="">Tresses libres</div>
+              <div className="">
+                <span className="text-neutral-400">45min • </span> 30€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Tresses libres{" "}
+                <span className="text-neutral-400">avec rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">1h • </span> 35€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">Vanilles</div>
+              <div className="">
+                <span className="text-neutral-400">45min • </span> 35€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Vanilles <span className="text-neutral-400">avec rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">2h • </span> 40€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">Tresses collées simple</div>
+              <div className="">
+                <span className="text-neutral-400">45min • </span> 30€
+              </div>
+            </li>
+            <li className="flex justify-between">
+              <div className="">
+                Tresses collées simple{" "}
+                <span className="text-neutral-400">avec rajouts</span>
+              </div>
+              <div className="">
+                <span className="text-neutral-400">2h • </span> 40€
+              </div>
+            </li>
+          </ul>
+        ),
+      },
+      {
         question: "Abonnement coupe",
         answer: (
           <ul className="space-y-4 text-sm px-1">
@@ -263,7 +445,9 @@ export default function Price() {
                       value={faq.question}
                       className="w-full"
                     >
-                      <AccordionTrigger className="text-xl">{faq.question}</AccordionTrigger>
+                      <AccordionTrigger className="text-xl">
+                        {faq.question}
+                      </AccordionTrigger>
                       <AccordionContent>{faq.answer}</AccordionContent>
                     </AccordionItem>
                   ))}
