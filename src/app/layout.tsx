495a43eb54234337a484b8a6aca2f6b1ad3f0096
ASSANI Younes <younesassani@gmail.com>
2024-10-23 16:37:01 +0200

    add Analytics & Speed Insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
@@ -12,12 +14,22 @@ export const metadata: Metadata = {
   title: "Quality Barber",
   description: "Salon de coiffure professionnelle sur Dunkerque",
   icons: [
-    {rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png"},
-    {rel: "icon", type:"image/png", sizes:"32x32" , url: "/favicon-32x32.png"},
-    {rel: "icon", type:"image/png", sizes:"16x16" , url: "/favicon-16x16.png"},
-    {rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#5bbad5"},
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#5bbad5" },
   ],
-  authors: [{name: "You-Dev", url: "https://github.com/Younesasn"}],
+  authors: [{ name: "You-Dev", url: "https://github.com/Younesasn" }],
 };
 
 export default function RootLayout({
@@ -34,6 +46,8 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
